2aeded411659688207611ebc00b32685d91578a2
Gustavo Morales <A00828432@tec.mx>
2026-02-18 20:08:31 -0600

    data files initial commit

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+"""Reservation System"""
+
+import json
+
+class Hotel():
+    def hola():
+        return
+        
+class Customer():
+    def hola2():
+        return
+
+class Reservations():
+    def hola3():
+        return
